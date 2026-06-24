939feebc58caa9dc9386194babbafffa1b7ced9c
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-06-24 16:41:32 -0300

    Criando o middleware para obter o body na req

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import http from 'node:http';
 
-function listener(req, res) {
-  //Função
+import { jsonHandler } from './middlewares/jsonHandler.js';
+
+async function listener(req, res) {
+  await jsonHandler(req, res);
+
+  console.log(req.body);
 }
 
 http.createServer(listener).listen(3333);
