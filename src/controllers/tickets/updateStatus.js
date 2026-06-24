eb1092b4cf9a5e201feb17d7f8675271d7023f98
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-06-24 19:40:50 -0300

    Adicionando a solução de fechamento do ticket.

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,7 +1,8 @@
 export function updateStatus({ req, res, database }) {
   const { id } = req.params;
+  const { solution } = req.body;
 
-  database.update('tickets', id, { status: 'closed' });
+  database.update('tickets', id, { status: 'closed', solution });
 
   return res.end();
 }
